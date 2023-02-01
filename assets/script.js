99b907d038d4f030e24b6b3dde906bb5e8acb4a6
d-hm <dahamer@bu.edu>
2023-01-31 20:57:27 -0500

    First upload from 23-01-21 - no changes

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    $(".ddb").on('click', function() {
+        $(this).siblings(".ddh").slideToggle(600);
+    });
+
+    $(".vimeovideowrap").fitVids();
+
+    // $(".ddb").on('click', function() {
+    //     $(this).siblings('.ddg').toggleClass('ddt');
+    // });
+
+})
+
+$(document).ready(function(){
+    $(".btn-all").click(function(){
+    //   $(".ddg").fadeOut();
+      $(".ddg").fadeIn();
+    });
+
+    $(".btn-draw").click(function(){
+        $(".ddg").fadeOut();
+        $(".draw").slideDown(3000);
+    });
+
+    $(".btn-graf").click(function(){
+        $(".ddg").fadeOut();
+        $(".graf").slideDown(3000);
+    });
+
+    $(".btn-video").click(function(){
+        $(".ddg").fadeOut();
+        $(".video").slideDown(3000);
+    });
+
+    $(".btn-fash").click(function(){
+        $(".ddg").fadeOut();
+        $(".fash").slideDown(3000);
+    });
+
+    $(".btn-misc").click(function(){
+        $(".ddg").fadeOut();
+        $(".misc").slideDown(3000);
+    });
+  });
